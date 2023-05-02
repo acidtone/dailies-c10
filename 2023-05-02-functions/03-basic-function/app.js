14dd3413aafb5ab86614a37724a51e696aea41ae
Tony Grimes <acidtone@tonygrimes.com>
2023-05-02 15:56:16 -0600

    added end-of-day code

---
+++ 2023-05-02-functions/03-basic-function/app.js
@@ -0,0 +1,14 @@
+const name = 'Tony';
+
+const name2 = 'Zee';
+
+const greet = (name) => {
+  const exclamation = 'Hi';
+  return `${exclamation}!, ${name}`
+}
+
+console.log(greet(name));
+
+console.log(greet(name2));
+
+console.log(exclamation);
